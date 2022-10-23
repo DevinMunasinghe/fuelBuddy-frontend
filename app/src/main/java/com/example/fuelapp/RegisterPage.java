d6ba8ba2715152f994d01833949e55cd50e84808
devin <devinmunasinghe@gmail.com>
2022-10-23 12:58:43 +0530

    register-button-ui-and-update-page-id-fixed

--- app/src/main/java/com/example/fuelapp/RegisterPage.java
+++ app/src/main/java/com/example/fuelapp/RegisterPage.java
@@ -6,11 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class RegisterPage extends AppCompatActivity {
 
     Button driverButton;
     Button ownerButton;
+
+    TextView loginLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -19,6 +22,34 @@ public class RegisterPage extends AppCompatActivity {
 
         driverButton = findViewById(R.id.driverBtn);
         ownerButton = findViewById(R.id.stationOwnerBtn);
+        loginLink = findViewById(R.id.loginPageLink);
+
+        driverButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(RegisterPage.this,DriverRegistration.class);
+                startActivity(intent);
+
+            }
+        });
+
+        ownerButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(RegisterPage.this,StationOwnerRegistration.class);
+                startActivity(intent);
+
+            }
+        });
+
+        loginLink.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(RegisterPage.this,MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
 
     }
 }
