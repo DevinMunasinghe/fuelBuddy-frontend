c52d5fd015310d337cc4001a43606dde6c24f545
devin <devinmunasinghe@gmail.com>
2022-10-25 14:24:20 +0530

    splash-screen-doing

--- app/src/main/java/com/example/fuelapp/SplashScreen.java
+++ app/src/main/java/com/example/fuelapp/SplashScreen.java
@@ -2,13 +2,30 @@ package com.example.fuelapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
+
+    TextView LabelFlash;
+    private static int time = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        LabelFlash = findViewById(R.id.flashlabel);
+        LabelFlash.setText(getIntent().getStringExtra("name"));
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },time);
     }
 }
